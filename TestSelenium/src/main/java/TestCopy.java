0760f694801daf03b19ebbcb69dc3be10ae3a161
xuchenCN <chenxu198511@gmail.com>
2014-09-28 17:26:14 +0800

    add sfss

--- TestSelenium/src/main/java/TestCopy.java
+++ TestSelenium/src/main/java/TestCopy.java
@@ -1,6 +1,7 @@
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.util.Calendar;
 
 
 public class TestCopy {
@@ -11,10 +12,10 @@ public class TestCopy {
    */
   public static void main(String[] args) throws Exception {
     Thread.sleep(3000);
-    Robot r = new Robot();
+//    Robot r = new Robot();
 //    r.keyPress(KeyEvent.VK_META);
-    r.keyPress(KeyEvent.VK_4);
-    r.keyRelease(KeyEvent.VK_4);
+//    r.keyPress(KeyEvent.VK_4);
+//    r.keyRelease(KeyEvent.VK_4);
 //    r.keyRelease(KeyEvent.VK_META);
     
 
